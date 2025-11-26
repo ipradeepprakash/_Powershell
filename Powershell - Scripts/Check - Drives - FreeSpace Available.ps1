7befb5699c7f457dc13ca58804853e8f4228fbe9
Pradeep Prakash <pradeep_prakash@outlook.com>
2025-11-26 10:57:22 +0530

    testing script

--- Powershell - Scripts/Check - Drives - FreeSpace Available.ps1
+++ Powershell - Scripts/Check - Drives - FreeSpace Available.ps1
@@ -1,13 +1,13 @@
 ﻿cls
-$h = hostname
-Write-Host "Server Name - $h"
+        $h = hostname
+        Write-Host "Server Name - $h"
 
-Get-Volume | ForEach-Object {
-$DriveL = $_.DriveLetter
-$sizeAlloc = [math]::round(($_.size / 1GB),2)
-$sizeRem = [math]::round(($_.SizeRemaining / 1GB),2)
+        Get-Volume | ForEach-Object {
+        $DriveL = $_.DriveLetter
+        $sizeAlloc = [math]::round(($_.size / 1GB),2)
+        $sizeRem = [math]::round(($_.SizeRemaining / 1GB),2)
 
-Write-Host "Drive - $DriveL | Available Size - $sizeAlloc | FreeSpace - $sizeRem "
+        Write-Host "Drive - $DriveL | Available Size - $sizeAlloc | FreeSpace - $sizeRem "
 
-}
+        }
 
