16fef2f6e46329780bc953b0a3116a36ca73279f
Pradeep Prakash <pradeep_prakash@outlook.com>
2025-12-08 19:52:32 +0530

    adding dbatools commands

--- dbatools/dbatool.ps1
+++ dbatools/dbatool.ps1
@@ -1,3 +1,17 @@
-
 # 0. Check your PowerShell version (v3+ required for Windows, Core 7.4+ for Linux/macOS)
 $PSVersionTable.PSVersion
+
+# 1. Install dbatools (takes about 30 seconds)
+Install-Module dbatools -Scope CurrentUser
+
+
+
+
+
+
+
+
+
+
+
+
